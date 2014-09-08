d93efa5fdcef3c0cd700be9df43659c85ee55605
Raphael Astrow <rastrow@andrew.cmu.edu>
2014-09-08 01:19:29 -0400

    Only 1 line changed - database password

--- src/servlets/DatabaseMethods.java
+++ src/servlets/DatabaseMethods.java
@@ -34,7 +34,7 @@ public class DatabaseMethods {
 	private String dbName = "disasterevacuationdb";
 	private String driver = "com.mysql.jdbc.Driver";
 	private String userName = "root";
-	private String password = "";
+	private String password = "1234";
 	private boolean isOpen = false;
 
 	public void open()
